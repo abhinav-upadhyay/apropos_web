009975791fcd6a1cfe5e89d54ddf09ea77cf76ae
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2016-03-30 04:39:22 +0530

    Add an absolute path to apropos so as to avoid confusion in future

--- apropos_web/config.py
+++ apropos_web/config.py
@@ -2,4 +2,4 @@
 
 MAN_DB_PATH = '/var/man.db'
 APROPOS_WEB_DB_PATH = '/usr/local/apropos_web/apropos_web.db'
-APROPOS_PATH='apropos'
+APROPOS_PATH='/usr/local/bin/apropos' #set a symlink to the compiled apropos
