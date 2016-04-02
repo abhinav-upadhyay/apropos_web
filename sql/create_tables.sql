452e54acc1182302281e163b08a393d2ae2ccf01
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2016-04-02 22:08:24 +0530

    Add if not exists to the create table statements

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,13 +1,13 @@
-create table click_log(page_name TEXT, section text, rank INT, query text, ip text,
+create table if not exists click_log(page_name TEXT, section text, rank INT, query text, ip text,
     platform text, browser text, version text, language text,
     referrer text, click_time int);
 
 
-create table query_log(query text, previous_query text, ip text, platform text, browser text, version text, language text, referrer text,
+create table if not exists query_log(query text, previous_query text, ip text, platform text, browser text, version text, language text, referrer text,
 query_time int);
 
-create table page_visit_log(page_id int, ip text, platform text, browser text, version text, language text, visit_time int);
+create table if not exists page_visit_log(page_id int, ip text, platform text, browser text, version text, language text, visit_time int);
 
-create table pages(page_id int, page_name text);
+create table if not exists pages(page_id int primary key, page_name text);
 
 insert into pages(page_id, page_name) values(1, 'index.html');
