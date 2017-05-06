3e59c87a073ff1f84a8ffab4594d8942a00c0620
Abhinav Upadhyay <abhinav@NetBSD.org>
2017-05-06 18:23:37 +0000

    Add more releases and use pretty names.

--- apropos_web/config.py
+++ apropos_web/config.py
@@ -8,13 +8,13 @@ DISTANCE_PATH='/usr/local/bin/similar_words'
 BOW_FILE='/usr/local/apropos_web/bow.bin'
 SGRAM_FILE='/usr/local/apropos_web/sgram.bin'
 DB_PATHS={
-        'netbsd-6': '/usr/local/apropos_web/netbsd-6/man.db',
-        'netbsd-6-0': '/usr/local/apropos_web/netbsd-6-0/man.db',
-        'netbsd-6-1': '/usr/local/apropos_web/netbsd-6-1/man.db',
-        'netbsd-7': '/usr/local/apropos_web/netbsd-7/man.db',
-        'netbsd-7-0': '/usr/local/apropos_web/netbsd-7-0/man.db',
-        'netbsd-7-1': '/usr/local/apropos_web/netbsd-7-1/man.db',
-        'netbsd-current': '/usr/local/apropos_web/HEAD/man.db',
+        'NetBSD-6': '/usr/local/apropos_web/netbsd-6/man.db',
+        'NetBSD-6-0': '/usr/local/apropos_web/netbsd-6-0/man.db',
+        'NetBSD-6-1': '/usr/local/apropos_web/netbsd-6-1/man.db',
+        'NetBSD-7': '/usr/local/apropos_web/netbsd-7/man.db',
+        'NetBSD-7-0': '/usr/local/apropos_web/netbsd-7-0/man.db',
+        'NetBSD-7-1': '/usr/local/apropos_web/netbsd-7-1/man.db',
+        'NetBSD-current': '/usr/local/apropos_web/HEAD/man.db',
         'posix':'/usr/local/apropos_web/posix/man.db',
-        'openbsd':'/usr/local/apropos_web/openbsd/man.db',
-        'linux':'/usr/local/apropos_web/linux/man.db'}
+        'OpenBSD':'/usr/local/apropos_web/openbsd/man.db',
+        'Linux':'/usr/local/apropos_web/linux/man.db'}
