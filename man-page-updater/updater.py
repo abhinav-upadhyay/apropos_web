11164793b04b83ba90c573195c65c42f4e225fd0
Abhinav Upadhyay <abhinav@NetBSD.org>
2017-05-07 15:34:37 +0000

    Also need to add \n at the end of the _mandb entry, otherwise the man.conf parser complains.

--- man-page-updater/updater.py
+++ man-page-updater/updater.py
@@ -140,7 +140,7 @@ def run_makemandb(directory, release_name):
     print('Copying man.conf to %s' % directory)
     shutil.copy(HOME_DIR + '/man.conf', directory)
     with open(directory + '/man.conf', 'a') as f:
-        f.write('_mandb %s' %  (mandb_copy_dir + '/man.db'))
+        f.write('_mandb %s\n' %  (mandb_copy_dir + '/man.db'))
 
     os.environ['MANPATH'] = directory
     proc = subprocess.Popen(['makemandb', '-C', directory + '/man.conf', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
