a586fe0202aea8a9d9d321ac82e0f3595025dbff
Abhinav Upadhyay <abhinav@NetBSD.org>
2017-05-11 20:38:31 +0000

    Add freebsd man page indexing support

--- man-page-updater/freebsd.py
+++ man-page-updater/freebsd.py
@@ -1,14 +1,48 @@
-NYCDN_URL = 'http://nycdn.netbsd.org/pub/NetBSD-daily/'
-AMD64_SETS_URL = 'amd64/binary/sets/'
-HISTORY_FILE = '/var/mandb_updates.log'
-monitored_targets = {}
-monitored_targets['NetBSD-6'] = 'netbsd-6/'
-monitored_targets['NetBSD-6-0'] = 'netbsd-6-0/'
-monitored_targets['NetBSD-6-1'] = 'netbsd-6-1/'
-monitored_targets['NetBSD-7'] = 'netbsd-7/'
-monitored_targets['NetBSD-7-0'] = 'netbsd-7-0/'
-monitored_targets['NetBSD-7-1'] = 'netbsd-7-1/'
-monitored_targets['NetBSD-current'] = 'HEAD/'
+from __future__ import print_function
+from updater_utils import *
+from bs4 import BeautifulSoup
+import requests
+import sys
+import time
 
-base_set_names = ['base.tgz', 'comp.tgz', 'games.tgz', 'man.tgz', 'text.tgz']
-xset_names = ['xbase.tgz', 'xcomp.tgz', 'xetc.tgz', 'xfont.tgz', 'xserver.tgz']
+BASE_URL = 'http://download.freebsd.org/ftp/snapshots/amd64/'
+AMD64_SETS_URL = 'amd64/'
+HISTORY_FILE = '/var/openbsd_mandb_updates.log'
+TAR_OPTIONS = '-xzf'
+monitored_targets = {}
+monitored_targets['FreeBSD-11.0'] = '11.0-STABLE/'
+monitored_targets['FreeBSD-10.3'] = '10.3-STABLE/'
+monitored_targets['FreeBSD-12.0'] = '12.0-STABLE/'
+
+base_set_names = ['base.txz']
+
+def get_base_setnames(release_name):
+    return base_set_names
+
+
+def get_latest_sets_url(release_url, previous_build_date=None):
+    #unlike Net or Open, here we directly use the base URL
+    r = requests.get(BASE_URL)
+    if r.status_code < 200 or r.status_code > 299:
+        eprint('Error from URL: %s' % release_url)
+        return None, None
+
+    soup = BeautifulSoup(r.text)
+    table = soup.find('table')
+
+    for row in table.find_all('tr')[1:]:
+        col = row.find_all('td')
+        release_name = col[0].string.strip()
+        if release_name not in monitored_targets.values():
+            continue
+        date_str = col[2].string.strip()
+        t = time.strptime(date_str, '%Y-%b-%d %H:%M')
+        nseconds = int(time.mktime(t))
+        if nseconds > previous_build_date:
+            return release_url, nseconds
+        else:
+            return None, None
+    else:
+        return None, None
+
+    return None, None
