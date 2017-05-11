14205fb00b0691c35c6bd6470e20210179901365
Abhinav Upadhyay <abhinav@NetBSD.org>
2017-05-11 18:19:38 +0000

    Use a different mirror as the CDN mirror seems to be not behaving well Fix some slash issues in the URLs Remove 5.9 release Use %s in set names instead of %d

--- man-page-updater/openbsd.py
+++ man-page-updater/openbsd.py
@@ -4,22 +4,19 @@ import requests
 import sys
 import time
 
-BASE_URL = 'http://mirrors.evowise.com/pub/OpenBSD/'
+BASE_URL = 'https://ftp3.usa.openbsd.org/pub/OpenBSD/'
 AMD64_SETS_URL = 'amd64/'
 HISTORY_FILE = '/var/openbsd_mandb_updates.log'
 monitored_targets = {}
 monitored_targets['OpenBSD-6.1'] = '6.1/'
-monitored_targets['OpenBSD-6-0'] = '6.0/'
-monitored_targets['OpenBSD-5-9'] = '5.9/'
+monitored_targets['OpenBSD-6.0'] = '6.0/'
 
-base_set_names = ['base%d.tgz', 'comp%d.tgz', 'game%d.tgz', 'man%d.tgz']
+base_set_names = ['base%s.tgz', 'comp%s.tgz', 'game%s.tgz', 'man%s.tgz']
 
 def get_base_setnames(release_name):
-    release_number = ''
-    for c in release_name:
-        if c == '.':
-            continue
-        release_number += c
+    release_number = release_name.split('-')[1]
+    release_number_parts = release_number.split('.')
+    release_number = ''.join(release_number_parts)
     names = [(setname % release_number) for setname in base_set_names]
     return names
 
@@ -37,10 +34,10 @@ def get_latest_sets_url(release_url, previous_build_date=None):
         if line.find('amd64') != -1:
             date_parts = line.split('</a>')[1].strip().split()[0:2]
             date_str = date_parts[0] + ' ' + date_parts[1]
-            t = time.strptime(date_str, '%d-%M-%Y %H:%M')
+            t = time.strptime(date_str, '%d-%b-%Y %H:%M')
             nseconds = int(time.mktime(t))
             if nseconds > previous_build_date:
-                return release_url + '/amd64', nseconds
+                return release_url + 'amd64/', nseconds
             else:
                 return None, None
 
