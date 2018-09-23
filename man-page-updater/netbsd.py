66d5173c74aa6e06c31819acf5f3e6fce6f5c8c8
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2018-09-23 19:10:27 +0530

    Ignore latest build in the build list for netbsd.
    We only want to work with the timestamps

--- man-page-updater/netbsd.py
+++ man-page-updater/netbsd.py
@@ -37,6 +37,8 @@ def get_latest_sets_url(release_url, previous_build_date=None):
         return None, None
 
     for date_str in reversed(dates):
+        if date_str == 'latest':
+            continue
         date = int(date_str[:-2])
         if previous_build_date:
             if date > previous_build_date:
